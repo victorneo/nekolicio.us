f18a439acb9e47bf58af95eb89ab42b0a6ff305d
Victor Neo <icyisamu@gmail.com>
2022-11-07 23:10:43 +0800

    Fix publish.sh

--- publish.sh
+++ publish.sh
@@ -7,22 +7,18 @@ then
 fi
 
 echo "Deleting old publication"
-rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
-
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
-
-echo "Removing existing files"
-rm -rf public/*
+git rm -rf docs
 
 echo "Generating site"
 hugo
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+echo "Renaming public to docs"
+mv public docs
+
+echo "Adding docs"
+git add -f docs
+
+git commit -m "Publishing to gh-pages (publish.sh)"
 
 #echo "Pushing to github"
 git push origin gh-pages
